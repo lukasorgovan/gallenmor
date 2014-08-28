8936bf2e61f863185cb2ade2cc0647eb8d1a784f
Adrian Huna <huna.adrian@gmail.com>
2014-08-28 19:53:08 +0200

    Generator messages fix

--- assets/js/generator.js
+++ assets/js/generator.js
@@ -11,18 +11,16 @@ $('#generator').on('submit', function(event) {
 				url: "/api/generator",
 				data: {name: formName, race: formRace}
 			}).done(function ( msg ) {
-
-				$('#msg').show().removeClass('errorMessage').addClass('successMessage').html(msg);
-
+				$('#msg').removeClass('errorMessage').addClass('successMessage').html(msg).show();
 			}).fail(function () {
-				$('#msg').removeClass('successMessage').html('Pridávanie nových mien dočasne nefunguje. Skús to neskôr.');
+				$('#msg').removeClass('successMessage').addClass('errorMessage').html('Pridávanie nových mien dočasne nefunguje. Skús to neskôr.').show();
 			});	
 		}
 		else {
-			$('#msg').removeClass('successMessage').addClass('errorMessage').html('Meno má nesprávny tvar. Musí mať 2-3 slová (každé slovo musí mať aspon 2 písmená) a môže obsahovať apostrof \'. Príklady: Alegreya Growe, Morgana la Fei, Al \'Sahib');
+			$('#msg').removeClass('successMessage').addClass('errorMessage').html('Meno má nesprávny tvar. Musí mať 2-3 slová (každé slovo musí mať aspon 2 písmená) a môže obsahovať apostrof \'. Príklady: Alegreya Growe, Morgana la Fei, Al \'Sahib').show();
 		}
 	}
 	else {
-		$('#msg').removeClass('successMessage').addClass('errorMessage').html('Meno nesmie byť dlhšie ako 50 znakov');
+		$('#msg').removeClass('successMessage').addClass('errorMessage').html('Meno nesmie byť dlhšie ako 50 znakov').show();
 	}
 });
