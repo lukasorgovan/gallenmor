c945f58ca71f8f106080886607194dddbfa6c1a4
Adrian Huna <huna.adrian@gmail.com>
2014-09-02 22:40:26 +0200

    view fix

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -1,4 +1,4 @@
-<?php $ci = get_instance(); $ci->load->view('header', array('bodyClass' => 'e404')); ?>
+<?php $ci = get_instance(); $ci->load->view('layout/index/header', array('bodyClass' => 'e404')); ?>
 
 	<div id="container">
 		<h1>Objavil si sa na neznámom miesite s bolesťou v hlave. Jediné čo vidíš je svetlo z portálu. Vstúp doň a zachráň sa pred prázdnotou.</h1>
